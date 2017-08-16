ac73e082de34d9cd0ffc51697caff56b6e60123b
Yoni Bettan <yonibettan@gmail.com>
2017-08-16 10:49:36 +0300

    adding setup.sh script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+       
+
+# creates soft links to all file 
+ln -s -f ~/.my_dotfiles/.bashrc ~/.bashrc 
+ln -s  ~/.my_dotfiles/.aliases ~/.aliases 
+ln -s  ~/.my_dotfiles/.vimrc ~/.vimrc 
+ln -s  ~/.my_dotfiles/.launchers ~/.launchers 
+ln -s  ~/.my_dotfiles/.tmux.conf ~/.tmux.conf 
+	       
+
+# source .bashrc file
+if [ -e ~/.bashrc ] ; then
+    echo source ~/.bashrc...
+    source ~/.bashrc 
+fi
