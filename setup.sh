1e3c9630d0758238577ebaaae813c4b17215cca0
Yoni Bettan <yonibettan@gmail.com>
2017-08-16 10:53:08 +0300

    changed setup.sh to be interactive

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
        
 
 # creates soft links to all file 
-ln -s -f ~/.my_dotfiles/.bashrc ~/.bashrc 
-ln -s  ~/.my_dotfiles/.aliases ~/.aliases 
-ln -s  ~/.my_dotfiles/.vimrc ~/.vimrc 
-ln -s  ~/.my_dotfiles/.launchers ~/.launchers 
-ln -s  ~/.my_dotfiles/.tmux.conf ~/.tmux.conf 
+ln -s -i ~/.my_dotfiles/.bashrc ~/.bashrc 
+ln -s -i ~/.my_dotfiles/.aliases ~/.aliases 
+ln -s -i ~/.my_dotfiles/.vimrc ~/.vimrc 
+ln -s -i ~/.my_dotfiles/.launchers ~/.launchers 
+ln -s -i ~/.my_dotfiles/.tmux.conf ~/.tmux.conf 
 	       
 
 # source .bashrc file
