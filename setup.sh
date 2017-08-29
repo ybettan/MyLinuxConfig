290622b80f56bbae0a5e23358fa5a8359ab08638
ybettan <yonibettan@gmail.com>
2017-08-29 12:33:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,20 @@
        
 
 # creates soft links to all file 
-ln -s -i ~/.my_dotfiles/.bashrc ~/.bashrc 
-ln -s -i ~/.my_dotfiles/.aliases ~/.aliases 
-ln -s -i ~/.my_dotfiles/.vimrc ~/.vimrc 
-ln -s -i ~/.my_dotfiles/.launchers ~/.launchers 
-ln -s -i ~/.my_dotfiles/.tmux.conf ~/.tmux.conf 
+ln -s -i ~/MyLinuxConfig/dotfiles/bashrc ~/.bashrc 
+ln -s -i ~/MyLinuxConfig/dotfiles/aliases ~/.aliases 
+ln -s -i ~/MyLinuxConfig/dotfiles/vimrc ~/.vimrc
+ln -s -i ~/MyLinuxConfig/dotfiles/tmux.config ~/.tmux.config
+ln -s -i ~/MyLinuxConfig/dotfiles/launchers ~/.launchers
+
+### remove
+#ln -s -i ~/.my_dotfiles/.bashrc ~/.bashrc 
+#ln -s -i ~/.my_dotfiles/.aliases ~/.aliases 
+#ln -s -i ~/.my_dotfiles/.vimrc ~/.vimrc 
+#ln -s -i ~/.my_dotfiles/.launchers ~/.launchers 
+#ln -s -i ~/.my_dotfiles/.tmux.conf ~/.tmux.conf 
+
+
 	       
 
 # source .bashrc file
