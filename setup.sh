940699ffc5b501092406a7b8b63e442688e2e709
fedora <fedora@localhost.localdomain>
2017-09-05 15:47:48 +0300

    setup.sh:
    	change the scripts order so source .bashrc work on updated files
    
    Signed-off-by: fedora <fedora@localhost.localdomain>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash 
        
 
+# install basic programs
+sudo dnf install vim
+sudo dnf install tmux
+sudo dnf install ctags
+sudo dnf install cscope
+
+
+# install vim-plug for vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
 # creates soft links to all file 
 ln -s -i ~/MyLinuxConfig/dotfiles/bashrc ~/.bashrc 
 ln -s -i ~/MyLinuxConfig/dotfiles/aliases ~/.aliases 
@@ -14,14 +26,3 @@ if [ -e ~/.bashrc ] ; then
     echo source ~/.bashrc...
     source ~/.bashrc 
 fi
-
-
-# install basic programs
-sudo dnf install vim
-sudo dnf install tmux
-sudo dnf install ctags
-sudo dnf install cscope
-
-# install vim-plug for vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
