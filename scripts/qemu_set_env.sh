ca954265a4b4eb23d96f2d67bd7b473c7945deda
Yoni Bettan <ybettan@redhat.com>
2017-09-05 15:24:28 +0300

    scripts/qemu_set_env.sh:
    added comment to the scripts
    added project config like configure mailing list, cc etc'
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- scripts/qemu_set_env.sh
+++ scripts/qemu_set_env.sh
@@ -4,16 +4,25 @@
 
 
 
+# make sure i am not erasing all my chnges in accident
 if [[ -e ~/Work/Qemu/qemu ]]; then
     echo '~'/Work/Qemu/qemu is\'t empty!
     exit
 fi
 
+
+
+# remove all folder if existed and was backed up
 if [[ -e ~/Work/Qemu ]]; then
     rm -rf ~/Work/Qemu
 fi
 
+
+
+# so that script don't change the current directory
 original_dir=`pwd`;
+
+
 
 # create DIRs
 mkdir -p ~/Work/Qemu/qemu_bin/x86_64
@@ -22,18 +31,46 @@ if [[ $USER != ybettan  ]]; then
     mkdir  ~/Work/Qemu/qemu_bin/full
 fi
     
+mkdir  ~/Work/Qemu/patches
+
+
 # clone the project
 cd ~/Work/Qemu
 git clone git://git.qemu-project.org/qemu.git
+
+
+
+# set some project configuration
+cd qemu/
+
+# make patches more readable
+git config diff.renames true            
+git config diff.algorithm patience
+
+# connect to the mail server to be able to send mails
+git config sendemail.smtpServer smtp.corp.redhat.com
+
+# automatically CC the maintainer of the patch
+git config sendemail.cccmd 'scripts/get_maintainer.pl --nogit-fallback' 
+
+cd ../
+
+
 
 # preparation for compilation
 cd qemu_bin/x86_64
 ../../qemu/configure --target-list=x86_64-softmmu --enable-debug
 
+# in every machine that isn't host create also a full version
 if [[ $USER != ybettan  ]]; then
     cd ../full
     ../../qemu/configure --enable-debug
 fi
 
+
+
 # return to original dir
 cd $original_dir
+
+
+
