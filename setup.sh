8ef02868b4d02f0980b15f12ab6a7f18e5a78026
Yoni Bettan <ybettan@redhat.com>
2020-08-22 17:19:52 +0300

    Replacing Homebrew installer from Ruby to Bash.
    ```
    Warning: The Ruby Homebrew installer is now deprecated and has been
    rewritten in Bash. Please migrate to the following command:
    
    /bin/bash -c \
        "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
    ```
    
    Signed-off-by: Yoni Bettan <ybettan@redhat.com>

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ function install_packages {
     if [[ $os == "Darwin" ]]; then
 
         # instal Brew package manager
-        /usr/bin/ruby -e \
-            "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || \
+        /bin/bash -c \
+            "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" || \
             err=$?
 
         packageManager="brew"
