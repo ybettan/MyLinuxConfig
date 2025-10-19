48e6e818169718d040186aa800f4c0ebdaa22665
Yoni Bettan <yonibettan@gmail.com>
2025-10-19 21:54:23 +0300

    Adding the python provider configuration to the `jedi-vim` plugin.
    jedi-vim was requiring `pynvim` to be installed in order to work, I have
    asked claude to debug the error and he has:
    1. ✅ Installed pynvim: pip3 install pynvim
    2. ✅ Configured Python3 provider in the jedi-vim plugin's file
    
    Signed-off-by: Yoni Bettan <yonibettan@gmail.com>

--- dotfiles/nvim/lua/plugins/jedi-vim.lua
+++ dotfiles/nvim/lua/plugins/jedi-vim.lua
@@ -5,6 +5,8 @@ return {
     -- Optional: load the plugin only for python files.
     ft = "python",
     config = function()
+      -- Python3 provider configuration
+      vim.g.python3_host_prog = "/usr/bin/python3"
       -- Set the command for showing call signatures
       vim.g["jedi#show_call_signatures"] = 2
       -- Set the command for goto functionality
