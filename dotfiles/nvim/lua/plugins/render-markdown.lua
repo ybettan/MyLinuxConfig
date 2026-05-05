345dedd7ca16dfa3975789a95a1f6a4e7bd5ec3d
Yoni Bettan <yonibettan@gmail.com>
2026-05-05 16:22:59 +0300

    Added the `render-markdown` nvim plugin.
    This is rendering `.md` files like a web browser will.
    
    Signed-off-by: Yoni Bettan <yonibettan@gmail.com>

---
+++ dotfiles/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,15 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        'echasnovski/mini.icons',
+    },
+    ft = { 'markdown' },
+    opts = {
+        heading = {
+            icons = { '# ', '## ', '### ', '#### ', '##### ', '###### ' },
+        },
+        render_modes = { 'n', 'c', 't' },
+    },
+}
+
